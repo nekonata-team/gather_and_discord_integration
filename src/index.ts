520d546009ef11a134d8a8c980f399060ed063b1
tyPhoon <typhoonmoon1022@gmail.com>
2025-04-02 07:04:23 +0900

    fix: 不要な冗長な記述

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,14 @@ import { DiscordNotifier } from "./discord_notifier";
 import { GatherMonitor } from "./gather_monitor";
 
 function main() {
-	try {
-		console.log("Starting Gather Town Bot...");
+	console.log("Starting Gather Town Bot...");
 
-		console.log(GATHER.API_KEY, GATHER.SPACE_ID, GATHER.MAP_ID);
-		console.log(DISCORD.WEBHOOK_URL);
+	console.log(GATHER.API_KEY, GATHER.SPACE_ID, GATHER.MAP_ID);
+	console.log(DISCORD.WEBHOOK_URL);
 
-		const notifier = new DiscordNotifier(DISCORD.WEBHOOK_URL);
-
-		const monitor = new GatherMonitor(
-			GATHER.SPACE_ID,
-			GATHER.API_KEY,
-			notifier,
-		);
-		notifier.sendSystemNotification("オフィスの入退室監視を開始しました");
-		monitor.start();
-	} catch (error) {
-		console.error("Error starting Gather Town Bot:", error);
-		process.exit(1);
-	}
+	const notifier = new DiscordNotifier(DISCORD.WEBHOOK_URL);
+	const monitor = new GatherMonitor(GATHER.SPACE_ID, GATHER.API_KEY, notifier);
+	monitor.start();
 }
 
 main();
